d2b5d35a97978db848db4e40b1e9f9a8bb9a9a39
Santosh Thapa <write2santosh@hotmail.com>
2019-04-16 12:23:16 +0100

    amended main.py file

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import json
 api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=4&convert=USD&CMC_PRO_API_KEY=44240872-6952-4dd7-997c-b4b8a64f43e0")
 
 api=json.loads(api_request.content)
+api=json.loads(api_request.content)
+api=json.loads(api_request.content)
+api=json.loads(api_request.content)
+
 
 print(api)
